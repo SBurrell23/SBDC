4089269e541be4ab0f2bdc31e70219088d054341
SBurrell23 <sburrell23@gmail.com>
2017-11-10 21:03:25 -0700

    get bigger and about section updates

--- pages/about_me/index.php
+++ pages/about_me/index.php
@@ -7,8 +7,13 @@
 			</div>
 			<div class="card-stacked">
 				<div class="card-content card-text">
-					<p>I'm 24 years old and a graduate of <a  target="_blank" href="http://www.cs.colostate.edu/cstop/index.php">Colorado State University</a> with a bachelor's in Computer Science and a minor in Business Administration. Currently I work as a software engineer doing mostly full stack web app development for <a  target="_blank" href="http://www.centurylink.com/">Century Link</a>. On this website you will find some programs and and web dev things I've made in my free time. I also use this site to compile photos from my trips and for a fun movie blog I do with my friends. If you don't know where to start, check out some of my <a href="?v=web_projects">web projects</a> and click on one that interests you!</p>
+					<p>I'm 24 years old and a graduate of <a  target="_blank" href="http://www.cs.colostate.edu/cstop/index.php">Colorado State University</a> with a bachelor's in Computer Science and a minor in Business Administration. Currently I work as a software engineer doing mostly full stack web app development for <a  target="_blank" href="http://www.centurylink.com/">CenturyLink</a>. On this website you will find programs, games and general web dev things I've made in my free time. I also use this site to compile photos from my trips and for a fun movie blog I do with my friends.<br><br>Check out some of my <a href="?v=web_projects">web projects</a> and find one that interests you. If you're still having trouble deciding you can start with some of my personal favorites below!</p>
 				</div>
+				<div class="card-action">
+		          <a href="?v=get_bigger">Get Bigger</a>
+		          <a href="?v=sliding_puzzle">Sliding Puzzle</a>
+		          <a href="?v=synonify">Synonify</a>
+		        </div>
 			</div>
 		</div>
 	</div>
